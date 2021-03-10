adb469282a9bc37e811a7ff0fd141143e00f7244
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-09 21:52:41 -0600

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "app_web" {
+  bucket = "${var.account_name}-app_web"
+  acl    = "private"                   
+}                                      
+
+resource "aws_s3_bucket_policy" "app_web" {
+  bucket = "${aws_s3_bucket.app_web.id}"
+  policy =<<POLICY
+{
+  "Id": "Policy1513880777555",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1513880773845",
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.app_web.arn}",
+      "Principal": {
+        "AWS": [
+          "arn:aws:iam::${var.account_id}:role/${var.app}-app_web",
+        ]
+      }
+    }
+  ]
+}
+POLICY
+}
