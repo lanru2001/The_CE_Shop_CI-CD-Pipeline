39e775f6d3fad843c35f3f41a4a03a0ee1fbec2d
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-10 12:40:19 -0600

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 yum update -y
-yum install apache2 -y
+yum install nginx -y
 cp /tmp/index.html /var/www/html/index.html
 chmod 755 /var/www/index.html
-service apache2 restart
+service nginx restart
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --reload
