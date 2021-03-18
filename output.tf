f4e8dcfb2c3db0d296798b14d165dd8799a76671
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-03-18 11:14:14 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,16 @@
 output "public_ip" {
-  value = "${aws_instance.webserver.public_dns}"
+  value = aws_instance.webserver.public_dns
 }
 
 output "vpc_id" {
   value = aws_vpc.app-vpc.id
 }
+
+output "instance_ips" {
+  value = aws_instance.webserver.*.public_ip
+}
+
+output "subnet_id" {
+  value = aws_instance.webserver.subnet_id
+}
+
