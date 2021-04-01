358ac70db53300725741b504ef94564ea7f5b00e
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-04-01 18:59:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -91,8 +91,8 @@ resource "aws_security_group" "instance" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
