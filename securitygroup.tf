12ea81d6b04b1256392e9d9416461ae422b30f54
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-11 18:15:28 -0500

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -1,1 +1,52 @@
+# Security group 
+resource "aws_security_group" "pipeline_security_group" {
+  name                      = "${local.module_prefix}-sg"
+  description               = "Allow traffic for instances"
+  vpc_id                    = aws_vpc.pipeline_vpc.id
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    =  -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = [
+      aws_vpc.pipeline_vpc   
+  ]
+}
