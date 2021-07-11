d526dc7c1e1594535424b6d82837d82215fc6715
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-11 18:24:07 -0500

    Update securitygroup.tf

--- securitygroup.tf
+++ securitygroup.tf
@@ -51,13 +51,35 @@ resource "aws_security_group" "pipeline_security_group" {
   ]
 }
 
-
 # prepare a security group for our load balancer my_alb.
 resource "aws_security_group" "my_alb_security_group" {
   vpc_id = aws_vpc.app-vpc.id
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# security group for launch config my_launch_configuration.
+resource "aws_security_group" "my_launch_config_security_group" {
+  vpc_id = aws_vpc.app-vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
